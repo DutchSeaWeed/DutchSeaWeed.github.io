f349cbf98978dcb1cc76c1aabc10d08b1ffc4229
ILoveRicardo <siebe.wiersma@sintlucasedu.nl>
2024-01-26 11:54:10 +0100

    added popstar energy cartidge

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,3 @@
-let console = document.getElementById("console2")
-let cartridge3 = document.getElementById("cartridge3Img")
-let cartridge4 = document.getElementById("cartridge3Img")
-let cartridge5 = document.getElementById("cartridge5Img")
-
 let LastPickup = 0;
 
 function allowDrop(ev) {
