6f4a230321600b39ff5c4eb9dbde1a9021a56c2a
ILoveRicardo <siebe.wiersma@sintlucasedu.nl>
2024-01-26 11:05:17 +0100

    partly added Popstar Energy

--- Script/script.js
+++ Script/script.js
@@ -1,100 +1,18 @@
-let testNumber = 10;
+let console = document.getElementById("console2")
+let cartridge3 = document.getElementById("cartridge3Img")
+let cartridge4 = document.getElementById("cartridge3Img")
+let cartridge5 = document.getElementById("cartridge5Img")
+
 let LastPickup = 0;
 
-//sessionStorage.setItem('language', 'english');
-
-function allowDrop(ev)
-{
+function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function drag(NewIndexToLoad)
-{
+function drag(NewIndexToLoad) {
     LastPickup = NewIndexToLoad;
 }
 
-function drop()
-{
+function drop() {
     window.location.href = 'index' + LastPickup + '.html';
 }
-
-function languageToDutch()
-{
-    sessionStorage.setItem('language', 'dutch');
-    let currentLanguage = sessionStorage.getItem('language');
-
-    var x = document.getElementsByClassName("en-text");
-    var i;
-    for (i = 0; i < x.length; i++)
-    {
-        x[i].style.display = 'none';
-    }
-
-    var x = document.getElementsByClassName("nl-text");
-    var i;
-    for (i = 0; i < x.length; i++)
-    {
-        x[i].style.display = 'block';
-    }
-}
-
-function languageToEnglish()
-{
-    sessionStorage.setItem('language', 'english');
-    let currentLanguage = sessionStorage.getItem('language');
-
-    var x = document.getElementsByClassName("nl-text");
-    var i;
-    for (i = 0; i < x.length; i++)
-    {
-        x[i].style.display = 'none';
-    }
-
-    var x = document.getElementsByClassName("en-text");
-    var i;
-    for (i = 0; i < x.length; i++)
-    {
-        x[i].style.display = 'block';
-    }
-}
-
-window.onload = function()
-{
-    let currentLanguage = sessionStorage.getItem('language');
-
-    let cartidge1 = document.getElementById('drag1');
-    let cartidge2 = document.getElementById('drag2');
-    let cartidge3 = document.getElementById('drag3');
-    let cartidge4 = document.getElementById('drag4');
-    let cartidge5 = document.getElementById('drag5');
-
-    if (currentLanguage === 'dutch')
-    {
-        var x = document.getElementsByClassName("en-text");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = 'none';
-        }
-
-        var x = document.getElementsByClassName("nl-text");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = 'block';
-        }
-    }
-    else
-    {
-        var x = document.getElementsByClassName("nl-text");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = 'none';
-        }
-
-        var x = document.getElementsByClassName("en-text");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = 'block';
-        }
-    }
-}
-
